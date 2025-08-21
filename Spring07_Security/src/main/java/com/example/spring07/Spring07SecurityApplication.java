10f1be7b34b410be1c56cadf1a7e66450a90aa2e
kimgura <oli999@naver.com>
2025-08-21 12:16:34 +0900

    Step07_Security 프로젝트 추가함

---
+++ Spring07_Security/src/main/java/com/example/spring07/Spring07SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07SecurityApplication.class, args);
+	}
+
+}
