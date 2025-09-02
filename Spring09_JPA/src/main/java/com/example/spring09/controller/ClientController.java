c886e2ac86b8f2e813efe79bc7486f97fd33065b
kimgura <oli999@naver.com>
2025-09-02 18:03:04 +0900

    Spring09 에 client 기능 구현중

--- Spring09_JPA/src/main/java/com/example/spring09/controller/ClientController.java
+++ Spring09_JPA/src/main/java/com/example/spring09/controller/ClientController.java
@@ -2,10 +2,16 @@ package com.example.spring09.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.spring09.dto.ClientDto;
 import com.example.spring09.service.ClientService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 /*
@@ -47,6 +53,61 @@ public class ClientController {
 		
 		// view page 에서 응답 
 		return "clients/list";
+	}
+	
+	@GetMapping("/clients/new")
+	public String newForm(Model model) {
+		//만일 "clientDto" 라는 키값으로 저장되어 있는 값이 model 객체에 없다면
+		if (!model.containsAttribute("clientDto")) {
+			//빈 ClientDto 객체라도 전달을 해 주어야 한다! 전달해 주지 않으면 thymeleaf 페이지에서 에러 발생 
+	        model.addAttribute("clientDto", new ClientDto());
+	    }
+		return "clients/new";
+	}
+	/*
+	 *  @Valid 어노테이션을 이용해서 dto 의 필드를 자동 검증하기 위한 의존 depenency 를 pom.xml 에 추가
+	 *  
+	    <dependency>
+		  <groupId>org.springframework.boot</groupId>
+		  <artifactId>spring-boot-starter-validation</artifactId>
+		</dependency>
+		
+		@Valid 가 dto 의 어떤 필드를 어떤 조건으로 검증할지 dto 클래스에 표시를 해야 한다 
+		
+		검증 결과가 BindingResult 객체에 담겨서 전달이 된다.
+		
+		@Valid 로 검증을 한 dto 매개변수 선언 바로 뒤에 BindingResult 매개변수를 선언해야 한다. 
+	 */
+	@PostMapping("/clients")
+	public String create(@Valid ClientDto dto, BindingResult br, RedirectAttributes ra) {
+		//폼 입력 내용중에 에러가 있는지(검증조건을 통과하지 못했는지) 여부를 알아내서
+		boolean hasError=br.hasErrors();
+		//만일 에러가 있다면
+		if(hasError) {
+			/*
+			 *  검증결과를 view page 에서 활용하려면 
+			 *  Model 객체 혹은 RedirectAttributes(리다일렉트 이동할경우) 객체에 정보를 담아 주어야 한다.
+			 *  
+			 *  model.addAttribute("key값", dto 객체);
+			 *  model.addAttribute("org.springframework.validation.BindingResult.key값", BindingResult 객체);
+			 *  
+			 *  or
+			 *  
+			 *  ra.model.addFlashAttribute("key값", dto 객체); 
+			 *  ra.model.addFlashAttribute("org.springframework.validation.BindingResult.key값", 
+			 *  	BindingResult 객체); 
+			 *  
+			 *  두개를 반드시 셋트로 "key값" 을 일치시켜서 담아야 한다 
+			 */
+			ra.addFlashAttribute("clientDto", dto);
+			ra.addFlashAttribute("org.springframework.validation.BindingResult.clientDto", br);
+			
+			return "redirect:/clients/new";
+		}
+		//새 고객 정보를 저장한다.
+		Long num = clientService.addClient(dto);
+		//고객 정보 자세히 보기로 리다일렉트 
+		return "redirect:/clients/"+num;
 	}
 }
 
--- Spring09_JPA/src/main/java/com/example/spring09/dto/ClientDto.java
+++ Spring09_JPA/src/main/java/com/example/spring09/dto/ClientDto.java
@@ -5,6 +5,9 @@ import java.time.LocalDateTime;
 
 import com.example.spring09.entity.Client;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +20,22 @@ import lombok.NoArgsConstructor;
 @Data
 public class ClientDto {
 	private Long num;
+	
+	@NotBlank(message="이름은 필수 입니다")
+	@Size(max=20, message="이름은 최대 20자까지 가능 합니다")
 	private String userName;
+	
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;
+	
+	/*
+	 *  @Past
+	 *  @PastOrPresent
+	 *  @Future 
+	 *  @FutrueOrPresent
+	 *  중에 하나로 검증할수 있다.
+	 */
+	@PastOrPresent(message = "생일은 미래일수 없습니다")
 	private LocalDate birthday;
 	
 	// static toDto() 메소드
