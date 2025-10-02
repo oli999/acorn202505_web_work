c7f2693692e7867f63322cb35b97e53a31ee1862
kimgura <oli999@naver.com>
2025-10-02 16:21:22 +0900

    Spring10 에 댓글 API 완성함

--- Spring10_JWT/src/main/java/com/example/spring10/service/CommentServiceImpl.java
+++ Spring10_JWT/src/main/java/com/example/spring10/service/CommentServiceImpl.java
@@ -21,7 +21,7 @@ public class CommentServiceImpl implements CommentService{
 	@Override
 	public CommentListResponse getComments(int parentNum, int pageNum) {
 		//한 페이지에 몇개씩 표시할 것인지
-		final int PAGE_ROW_COUNT=10;
+		final int PAGE_ROW_COUNT=3;
 		//하단 페이지를 몇개씩 표시할 것인지
 		final int PAGE_DISPLAY_COUNT=3;
 		//보여줄 페이지의 시작 ROWNUM
